1e70b17c29ed1c1f73a1c16fb02fa4ef80458c56
DimaKostrikin <dima.da222@yandex.ru>
2021-08-22 19:42:36 +0300

    исправил вызов root

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,6 @@
 import GitHubStore from 'store/GitHubStore';
 
-function App() {
-  const gitHubStore = new GitHubStore();
-
-  const EXAMPLE_ORGANIZATION = 'ktsstudio';
-
-  gitHubStore.getOrganizationReposList({
-    org: EXAMPLE_ORGANIZATION,
-    sort: 'created',
-    direction: 'desc',
-    per_page: 100,
-  }).then(result => {
-    console.log(result.data); 
-    // в консоли появится список репозиториев в ktsstudio
-  })
-  
+function App() {  
   return (
     <div>a</div>
   );
